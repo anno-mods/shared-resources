14dc23110ca1210c51d0a26354a27c9644a3e6aa
Jakob Harder <12190313+jakobharder@users.noreply.github.com>
2025-05-03 13:10:21 +0200

    add merge-tabs mod

--- .github/package.js
+++ .github/package.js
@@ -6,10 +6,14 @@ const mods = glob.sync("./out/*");
 mods.forEach(modPath => {
   const package = path.basename(modPath);
   const zipName = package
-    // .replace(/\[\w+\] /g, '')
     .replace(/\s/g, '-')
     .replace(/\[|\]|\(|\)/g, '')
-    .toLowerCase();
+    .toLowerCase()
+    // skip category for some
+    .replace('ui-', '')
+    // ModID and ModName don't match
+    .replace('fix-influence-tooltip', 'ToolTip_OtherResidences');
+
   child_process.execFileSync('tar', [
     '-c', '-a',
     '-f', `out/${zipName}.zip`,
